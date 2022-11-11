b85b37db48175cffea6b57514685b60ffeeefb24
lihaojie.1456 <lihaojie.1456@bytedance.com>
2022-11-11 19:01:31 +0800

    push 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 '''
-flask项目一般默认将静态的配置文件放在static目录里，将前端模板文件放在templates目录里
+flask项目一般默认将静态的配置文件放在static目录里, 将前端模板文件放在templates目录里
 '''
 
 from flask import Flask, redirect, url_for, render_template, request
@@ -17,11 +17,6 @@ app.config.from_object(config)
 def index():
     return "Index Page"
 
-# 渲染页面, 只需输入被渲染的文件名，flask会自动从template目录里查找
-@app.route("/index")
-def main_route():
-    return render_template("index.html")
-
 # 添加路由
 @app.route("/hello")
 def hello():
@@ -32,10 +27,28 @@ def hello():
 def show_username(username):
     return f"user name is {username}"
 
-# 通过路由传参并规定参数类型
+# 通过路由传参并规定参数类型, 支持string，int，float，path，uuid
 @app.route("/userid/<int:id>")
 def show_userid(id):
     return f"user id is {id}"
+
+# 渲染页面, 参数和对象, 只需输入被渲染的文件名，flask会自动从template目录里查找
+# 参数page_id, user_name, user, my_dict会被传到index.html文件中
+class User:
+    def __init__(self, username, email) -> None:
+        self.username = username
+        self.email = email
+my_dict = {"index":1, "feature":"good"}
+
+@app.route("/index/<int:id>")
+def main_page(id):
+    user = User(username="python语言", email="123@python.com")
+    return render_template("index.html", page_id=id, user_name="恭喜发财", user=user, my_dict=my_dict)
+
+# 渲染css，javascript的效果
+@app.route("/images")
+def images():
+    return render_template("image.html")
 
 # 通过关键字参数传参
 # book/list?page=100
@@ -56,7 +69,19 @@ def login():
     else:
         raise Exception("do not support this request method!")
 
- 
+# 接收POST请求中的form表单
+@app.route("/post_file", methods=["POST"])
+def recieve_file():
+    from PIL import Image 
+    import numpy as np 
+
+    id = request.form.get("id")
+    image = request.files.get("images")
+    image = Image.open(image)
+    image = np.array(image)
+    print(id, image)
+    return_dict = {"id":id, "image":str(image)}
+    return return_dict
 
 if __name__ == '__main__':
     app.run()
